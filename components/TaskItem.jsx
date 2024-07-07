89a3083944f1305d7c1f10c6bd7ab2e0ee7ce79a
zenik-kun <nikirukyun@gmail.com>
2024-07-07 15:25:20 +0530

    Edit, Delete button UI added.

--- components/TaskItem.jsx
+++ components/TaskItem.jsx
@@ -3,7 +3,7 @@ import React, { useRef } from 'react'
 import { GestureHandlerRootView, Swipeable } from 'react-native-gesture-handler'
 import { icons } from '../constants'
 
-const TaskItem = ({ task: {title, description}, onDone }) => {
+const TaskItem = ({ task: {title, description}, onDone, onEdit }) => {
 	const swipeableRef = useRef(null)
 
 	const handleSwipeableOpen = () => {
@@ -17,7 +17,7 @@ const TaskItem = ({ task: {title, description}, onDone }) => {
 	const rightSwipe = () => {
 		return (
 			<TouchableOpacity
-				className = "bg-cyan-200 rounded-2xl mb-2 mx-3 p-4 justify-center items-center"
+				className = "bg-cyan-200 shadow rounded-2xl mb-2 mx-3 p-4 justify-center items-center"
 				onPress = {() => {
 					swipeableRef.current.close();
 					onDone(id);
@@ -32,9 +32,42 @@ const TaskItem = ({ task: {title, description}, onDone }) => {
 		)
 	}
 
+	const leftSwipe = () => {
+		return (
+			<View className = "flex-row"> 
+				<TouchableOpacity
+					className = "bg-cyan-200 shadow rounded-2xl mb-2 ml-3 p-4 justify-center items-center"
+					onPress = {() => {
+						swipeableRef.current.close();
+						onDone(id);
+					}}
+				>
+					<Image 
+						source = {icons.del}
+						className = "h-10 w-10"
+						resizeMode = "contain"
+					/>
+				</TouchableOpacity>
+				<TouchableOpacity
+					className = "bg-cyan-200 shadow rounded-2xl mb-2 ml-2 mr-3 p-4 justify-center items-center"
+					onPress = {() => {
+						swipeableRef.current.close();
+						onEdit(id);
+					}}
+				>
+					<Image 
+						source = {icons.edit}
+						className = "h-10 w-10"
+						resizeMode = "contain"
+					/>
+				</TouchableOpacity>
+			</View>
+		)
+	}
+
 	return (
 		<GestureHandlerRootView>
-			<Swipeable renderRightActions = {rightSwipe} onSwipeableOpen = {handleSwipeableOpen} ref = {swipeableRef}>
+			<Swipeable renderLeftActions = {leftSwipe} renderRightActions = {rightSwipe} onSwipeableOpen = {handleSwipeableOpen} ref = {swipeableRef}>
 				<View className = "p-2 justify-center shadow mb-2 mx-3 rounded-2xl bg-gray-200">
 					<Text className = "font-lmedium text-base">
 						{title}
--- constants/icons.js
+++ constants/icons.js
@@ -1,5 +1,9 @@
-import done from "../assets/icons/done.png"
+import done from "../assets/icons/done.png";
+import edit from "../assets/icons/edit.png";
+import del from "../assets/icons/delete.png";
 
 export default {
     done,
+    edit,
+    del,
 }
